3275c76a5ab6999549eddd03a918acfabc00da77
mk <mkwong04@yahoo.com.sg>
2023-04-20 17:20:48 +0800

    upgrade to spring boot 3.x
    replace dependency springdoc-openapi-webflux-ui with
    springdoc-openapi-starter-webflux-ui

--- src/main/java/testm/Application.java
+++ src/main/java/testm/Application.java
@@ -1,22 +1,36 @@
 package testm;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 
 /**
  * 
  * Spring boot application main
  */
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Service API v1.0"))
+@OpenAPIDefinition
 public class Application {
 	/**
 	 * @param args 
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@Bean
+	public GroupedOpenApi serviceApi() {
+		String[] paths = {"/services/**"};
+		return GroupedOpenApi.builder()
+				.group("service")
+				.pathsToMatch(paths)
+				.build();
 	}
 }
